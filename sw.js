a28d459ae650e0155b371680811ad61adc64d284
ministiarkea <ministiarkea@gmail.com>
2026-08-13 14:30:06 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+/* Muutoskone - offline-välimuisti.
+   Nosta versionumeroa aina kun päivität index.html:n, muuten selain tarjoilee vanhaa. */
+const C = 'muutoskone-v12';
+const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-maskable.png', './apple-touch-icon.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(C).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== C).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  const u = new URL(e.request.url);
+  if (e.request.method !== 'GET' || u.origin !== location.origin) return;
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const cp = r.clone();
+      caches.open(C).then(c => c.put(e.request, cp)).catch(() => {});
+      return r;
+    }).catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
+  );
+});
